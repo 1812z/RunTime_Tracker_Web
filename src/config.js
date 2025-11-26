48e7f74e772135600d5d0f85c2fe5f2883376330
1812z <61372168+1812z@users.noreply.github.com>
2025-11-26 16:09:17 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const config = {
     dev: {
         API_BASE: import.meta.env.VITE_API_BASE || 'https://api-usage.1812z.top/api', // 默认开发环境
-        ADMIN_URL: import.meta.env.VITE_ADMIN_URL || 'https://runtime-tracker-admin.pages.dev/'
+        ADMIN_URL: import.meta.env.VITE_ADMIN_URL || 'https://runtime-tracker-admin.1812z.top/'
     },
     prod: {
         API_BASE: import.meta.env.VITE_API_BASE || 'https://api-usage.1812z.top/api', // 默认生产环境
-        ADMIN_URL: import.meta.env.VITE_ADMIN_URL || 'https://runtime-tracker-admin.pages.dev/'
+        ADMIN_URL: import.meta.env.VITE_ADMIN_URL || 'https://runtime-tracker-admin.1812z.top/'
     }
 };
 
